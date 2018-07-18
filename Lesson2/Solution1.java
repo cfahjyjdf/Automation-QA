84ab01e5fd96462ce2c493275887b32c0b666753
cfahjyjdf <safronova.inna2016@yandex.ru>
2018-07-18 20:14:58 +0300

    Lessons 1 Lesson1/Solution1.java string fix

--- Lesson2/Solution1.java
+++ Lesson2/Solution1.java
@@ -3,7 +3,7 @@ public class Solution1 {
 		String string="Рассерженный Родион Романович раскричался \"Рано радовались, размокший ремень рюкзака разорвался\"";
 		String string1=string.replace('р', '*');
 		//String string2=string.replace('Р', '*');
-		//System.out.printf( "%s\n", string1 );
-		System.out.printf( "%s\n", string2 );
+		System.out.printf( "%s\n", string1 );
+		//System.out.printf( "%s\n", string2 );
 	}
 }
